e3458e64267a515a6271d3348fc0a173e71ce846
Vituratic <marcel_kambeck@hotmail.de>
2018-10-23 13:30:04 +0200

    change at db

--- database/banka_logs.sql
+++ database/banka_logs.sql
@@ -26,7 +26,7 @@ CREATE TABLE `logs` (
   `lognumber` int(11) NOT NULL AUTO_INCREMENT,
   `user` int(11) NOT NULL COMMENT 'identifiziert über Kontonummer',
   `log` text,
-  `time` timestamp NULL DEFAULT NULL,
+  `time` datetime DEFAULT NULL,
   PRIMARY KEY (`lognumber`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -49,4 +49,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-10-23 10:54:56
+-- Dump completed on 2018-10-23 13:29:35
