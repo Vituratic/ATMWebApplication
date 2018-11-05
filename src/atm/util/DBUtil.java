bb4f0b26702e728d4a507ff783ec8d9e13d6edbe
Vituratic <marcel_kambeck@hotmail.de>
2018-11-05 11:38:25 +0100

    refactored db-connection

--- src/atm/util/DBUtil.java
+++ src/atm/util/DBUtil.java
@@ -2,14 +2,19 @@ package atm.util;
 
 import com.mysql.jdbc.Driver;
 
-import javax.swing.plaf.nimbus.State;
-import java.sql.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class DBUtil {
 
     private static final String URLHOLDER = "jdbc:mysql://localhost:3306/PLACEHOLDER?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-    private static final String USER = "root";
-    private static final String PASS = "Best_Team1337";
+    private static final String USER = readLoginData("USER");
+    private static final String PASS = readLoginData("PASS");
 
     public static boolean executeSql(final String sql, final String targetBank) {
         final String URL = URLHOLDER.replace("PLACEHOLDER", targetBank);
@@ -36,5 +41,25 @@ public class DBUtil {
         } catch (Exception e) {
             return null;
         }
+    }
+
+    private static String readLoginData(final String data) {
+        if (!data.equals("PASS") && !data.equals("USER")) {
+            return null;
+        }
+        try {
+            final File dataFile = new File("dblogin.txt");
+            BufferedReader br = new BufferedReader(new FileReader(dataFile));
+            String fromFile;
+            while ((fromFile = br.readLine()) != null) {
+                if (fromFile.startsWith(data)) {
+                    String[] result = fromFile.split("=");
+                    return result[1];
+                }
+            }
+        } catch (Exception e) {
+            return null;
+        }
+        return null;
     }
 }
