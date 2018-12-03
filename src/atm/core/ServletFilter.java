9d9057e6a531fee50ed7ca5fddfa5845a7b07fd6
Vituratic <marcel_kambeck@hotmail.de>
2018-12-03 13:36:44 +0100

    fixed clickjackability

---
+++ src/atm/core/ServletFilter.java
@@ -0,0 +1,25 @@
+package atm.core;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ServletFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("Filter init!");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("Filter destroy!");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.addHeader("X-Frame-Options", "DENY");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
