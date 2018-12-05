1345ad2c733f885cb20a41f626dfc4e0c628d615
Vituratic <marcel_kambeck@hotmail.de>
2018-12-05 07:25:03 +0100

    added header for xss-prot and content-sec-policy

--- src/atm/core/ServletFilter.java
+++ src/atm/core/ServletFilter.java
@@ -20,6 +20,8 @@ public class ServletFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
         response.addHeader("X-Frame-Options", "DENY");
+        response.setHeader("X-XSS-Protection", "1; mode=block");
+        response.setHeader("Content-Security-Policy", "default-src 'self'");
         filterChain.doFilter(servletRequest, servletResponse);
     }
 }
